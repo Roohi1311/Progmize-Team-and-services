32fee18b46d180f8680a625fd53caf97ae671f0d
Roohi1311 <raviha1311shaukat@gmail.com>
2026-07-12 13:49:35 +0500

    Add team members data and display functionality

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-// Progmize Portfolio Website
+const teamMembers = [
+    {
+        role: "CEO",
+        name: "Mohsin Rehmat",
+        image: "CEO.jpeg"
+    },
+    {
+        role: "Manager & Business Analyst",
+        name: "Ayesha Aghyaz",
+        image: "Manajor.jfif"
+    },
+    {
+        role: "HR",
+        name: "Areesha Umar",
+        image: "HR.jpeg"
+    },
+    {
+        role: "Frontend Developer",
+        name: "Hira Mithani",
+        image: "FrontEnd Developer.jpeg"
+    },
+    {
+        role: "Backend Developer",
+        name: "Gulab Ahmed",
+        image: "Backend Developer.jpeg"
+    },
+    {
+        role: "QA Tester",
+        name: "Raviha Shaukat",
+        image: "QA Tester.jpeg"
+    }
+];
+
+let index = 0;
+
+const empImage = document.getElementById("empImage");
+const empRole = document.getElementById("empRole");
+const empName = document.getElementById("empName");
+const teamCard = document.getElementById("teamCard");
+
+function showTeamMember(){
+
+    empImage.src = teamMembers[index].image;
+    empRole.textContent = teamMembers[index].role;
+    empName.textContent = teamMembers[index].name;
+
+    index++;
+
+    if(index >= teamMembers.length){
+        index = 0;
+    }
+}
+
+document.getElementById("teamBtn").addEventListener("click", function(){
+
+    teamCard.style.display = "block";
+
+    showTeamMember();
+
+    setInterval(showTeamMember, 3000);
+
+});
